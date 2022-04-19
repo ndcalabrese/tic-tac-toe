4d3cf2a75ff21620c94dbad59061f1ba465307cc
ndcalabrese <ndcalabrese@gmail.com>
2022-04-19 16:39:31 -0400

    Updates

---
+++ precache-manifest.ba76410589f8d53aa6467156c3c01beb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "569672a588d66a596542",
+    "url": "/tic-tac-toe/static/css/main.d6df7214.chunk.css"
+  },
+  {
+    "revision": "569672a588d66a596542",
+    "url": "/tic-tac-toe/static/js/main.569672a5.chunk.js"
+  },
+  {
+    "revision": "4de5f21f631e82a5a30f",
+    "url": "/tic-tac-toe/static/js/1.4de5f21f.chunk.js"
+  },
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "a5b93a7433ccbe64d781a08273e641e3",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
